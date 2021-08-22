004b39183990a2e8d2d08637e5162f0604dfc2b1
Ross Haliburton <ross.haliburton@raidiam.com>
2021-08-22 11:09:23 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-    loop {
-        println!("Guess the number!");
 
-        // Very secret :)
-        let secret_number = rand::thread_rng().gen_range(1..101);
-        println!("The secret number is: {}", secret_number);
-        
+    println!("Guess the number!");
+
+    // Very secret :)
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is: {}", secret_number);
+    loop {
         println!("Please input your guess.");
 
         let mut guess = String::new();
@@ -17,7 +17,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
