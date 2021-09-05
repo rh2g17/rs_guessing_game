23b9398019889861467a18919be668c3427f7f82
Ross Haliburton <ross.haliburton@raidiam.com>
2021-09-05 10:27:51 +0100

    improved readability for user and check for num

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,9 @@ use std::cmp::Ordering;
 
 fn main() {
 
+    let secret_number = rand::thread_rng().gen_range(1..101);
     println!("Guess the number!");
 
-    // Very secret :)
-    let secret_number = rand::thread_rng().gen_range(1..101);
-    
     loop {
         println!("Please input your guess.");
 
@@ -19,9 +17,11 @@ fn main() {
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Only numbers allowed.");
+                continue
+            }
         };
-        println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small"),
